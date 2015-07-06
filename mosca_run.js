1762963f5e79821f0e556d7bd18f42702b08104d
Linh Nguyen <B51960@freescale.com>
2015-07-06 15:24:22 +0700

    Update publish script and mosca config

---
+++ mosca_run.js
@@ -0,0 +1,44 @@
+/* Source: https://github.com/mcollina/mosca/wiki/Mosca-basic-usage */
+var mosca = require('mosca')
+
+var ascoltatore = {
+  //using ascoltatore
+  type: 'mongo',
+  url: 'mongodb://localhost:27017/mqtt',
+  pubsubCollection: 'ascoltatori',
+  mongo: {}
+};
+
+var moscaSettings = {
+  port: 1883,
+  backend: ascoltatore,
+  persistence: {
+    factory: mosca.persistence.Mongo,
+    url: 'mongodb://localhost:27017/mqtt'
+  }
+};
+
+var server = new mosca.Server(moscaSettings);
+server.on('ready', setup);
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+});
+
+// Fired when a client subscribed to broker
+server.on('subscribed', function(topic, client) {
+  // Check is topic required:
+  if (topic == '/expected') {
+    // Publish current values
+  }
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
+});
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running')
+}
--- publish-config.sh
+++ publish-config.sh
@@ -1,44 +1,44 @@
 #!/bin/sh
-mosquitto_pub -h m10.cloudmqtt.com -u secret -P secret -p 16585 -t denschu/home/config -m '[ {
+mosquitto_pub -h 10.207.215.177 -u linh -P secret -p 1883 -t linh/home/config -m '[ {
         "type" : "on_off",
         "value" : false,
         "name" : "Floorlamp 1",
-        "topic" : "denschu/home/devices/livingroom/floorlamp1/value",
+        "topic" : "linh/home/devices/livingroom/floorlamp1/value",
         "id" : "floorlamp1"
       }, {
         "type" : "dimmer",
         "value" : 0,
         "name" : "Floorlamp 2",
-        "topic" : "denschu/home/devices/livingroom/floorlamp2/value",
+        "topic" : "linh/home/devices/livingroom/floorlamp2/value",
         "id" : "floorlamp2"
       }, {
         "type" : "on_off",
         "value" : false,
         "name" : "Green Lamp",
-        "topic" : "denschu/home/devices/livingroom/green_lamp/value",
+        "topic" : "linh/home/devices/livingroom/green_lamp/value",
         "id" : "green_lamp"
       }, {
         "type" : "on_off",
         "value" : false,
         "name" : "Cabinet Light left",
-        "topic" : "denschu/home/devices/livingroom/cabinetlight_left/value",
+        "topic" : "linh/home/devices/livingroom/cabinetlight_left/value",
         "id" : "cabinetlight_left"
       }, {
         "type" : "on_off",
         "value" : false,
         "name" : "Cabinet Light right",
-        "topic" : "denschu/home/devices/livingroom/cabinetlight_right/value",
+        "topic" : "linh/home/devices/livingroom/cabinetlight_right/value",
         "id" : "cabinetlight_right"
       }, {
         "type" : "thermostat",
         "value" : 16,
         "name" : "Livingroom",
-        "topic" : "denschu/home/devices/livingroom/thermostat/value",
+        "topic" : "linh/home/devices/livingroom/thermostat/value",
         "id" : "thermostat_wohnzimmer"
       }, {
         "type" : "thermostat",
         "value" : 16,
         "name" : "Bathroom",
-        "topic" : "denschu/home/devices/bathroom/thermostat/value",
+        "topic" : "linh/home/devices/bathroom/thermostat/value",
         "id" : "thermostat_badezimmer"
       }]' -r
